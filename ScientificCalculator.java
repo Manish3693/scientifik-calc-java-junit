3d273aa89c25b61ad288ef857012bd53b1632ed5
Manish3693 <77501236+Manish3693@users.noreply.github.com>
2023-11-26 11:29:14 +0530

    Add files via upload

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -240,19 +240,20 @@ public class ScientificCalculator {
     private static void displayMenuScientific() {
         String[] menuItems = {
                 "Addition", "Subtraction", "Multiplication", "Division",
-                "Square Root", "Power", "Sin","Cos","Tan","Secant","Co-secant","Co-tangent", "Logarithm (Base 10)",
-                "Natural Logarithm", "Absolute Value", "Ceiling", "Floor", "Round",
-                "Logarithm (Custom Base)", "Factorial", "Exponential", "Angle Mode: Degrees/Radians",
-                "Degrees to Radians","Radians to Degrees",
-                "(a+b)^c", "Absolute Power", "Modulus", "Square", "Cube", "Minimum", "Maximum",
-                "Average", "Inverse", "Percentage", "Round Up", "Round Down", "Hyperbolic Sine",
-                "Hyperbolic Cosine", "Hyperbolic Tangent", "Arcsine", "Arccosine", "Arctangent",
-                "Permutation", "Combination", "Sum of n", "Sum of squares of n","Sum of cubes of n","Harmonic Number",
-                "Sum of kth power of n","Exit"
+                "Square Root", "Power", "Sin", "Cos", "Tan", "Secant", "Co-secant", "Co-tangent",
+                "Logarithm (Base 10)", "Natural Logarithm", "Logarithm (Base 2)", "Absolute Value",
+                "Ceiling", "Floor", "Round", "Logarithm (Custom Base)", "Factorial", "Exponential",
+                "Angle Mode Changed", "Degrees to Radians", "Radians to Degrees", "(a+b)^c",
+                "(a-b)^c", "Absolute Power", "Modulus", "Square", "Cube", "Minimum", "Maximum",
+                "Average", "Average", "Average", "Inverse", "Percentage", "Round Up", "Round Down",
+                "Hyperbolic Sine", "Hyperbolic Cosine", "Hyperbolic Tangent", "Arcsine", "Arccosine",
+                "Arctangent", "Permutation", "Combination", "Sum of n", "Sum of squares of n",
+                "Sum of cubes of n", "Sum of cubes n", "nth Harmonic number", "Exit"
         };
 
-        int columnWidth = 300;
-        int columns = 5;
+        // Rest of the code remains unchanged
+        int columnWidth = 30;
+        int columns = 3; // Adjust the number of columns as needed
         int rows = (int) Math.ceil((double) menuItems.length / columns);
 
         displayWelcomeScientific();
@@ -269,6 +270,7 @@ public class ScientificCalculator {
         }
         System.out.print("Enter your choice: ");
     }
+
 
     private static void displayWelcome(){
         System.out.println("Welcome to the Calculator Program");
@@ -293,14 +295,14 @@ public class ScientificCalculator {
                 "Subtraction",
                 "Multiplication",
                 "Division",
-                "Percentage"
+                "Exit"
         };
 
         int columnWidth = 25;
         int columns = 5;
         int rows = (int) Math.ceil((double) menuItems.length / columns);
 
-        displayWelcomeScientific();
+//        displayWelcomeScientific();
 
         System.out.println("Standard Calculator");
         for (int i = 0; i < rows; i++) {
